f8bf9dbc692c8bf2959e1bae4f4ddd0c302ebe9e
wiem <wiemgmc2021@gmail.com>
2021-03-24 13:25:44 +0100

    props checkpoint

---
+++ src/Profil.js
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import React from 'react'
+
+ const Profil = ({children,FullName, Bio, Profession,alertName}) => {console.log({children});
+   return (
+      <div>
+         <p>FullName is: {FullName}</p>
+         <p>Bio is: {Bio}</p>
+         <p >Profession is: {Profession}</p>
+         <img src= {children} alt="" onClick={()=>alertName(FullName)}></img>
+         
+      </div>
+   )
+}
+
+Profil.prototype ={
+   FullName: PropTypes.string,
+   Bio: PropTypes.string,
+   Profession: PropTypes.string,
+}
+
+Profil.defaultProps={
+   children:"https://cdn.pixabay.com/photo/2020/05/26/09/01/night-5222267_960_720.jpg",
+   FullName: "default name",
+   Bio: "Default Lorem",
+   Profession: "Ing",
+}
+export default Profil
